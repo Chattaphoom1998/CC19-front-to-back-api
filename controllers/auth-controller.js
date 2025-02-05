ac2f7a8c57255a677a8511b38ea8d23b5e2b1eab
Chattaphoom Kasisomang <chattaphoom.k@gmail.com>
2025-02-05 11:09:48 +0700

    Update controller, middleware, README, routes

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,6 +1,12 @@
 exports.register = (req, res, next) => {
 	//code
 	try {
+		//Step 1 req.body
+		console.log(req.body);
+		//Step 2 validate
+		//Step 3 check already
+		//Step 4
+
 		res.json({ message: "Hello register" });
 	} catch (error) {
 		next(error);
